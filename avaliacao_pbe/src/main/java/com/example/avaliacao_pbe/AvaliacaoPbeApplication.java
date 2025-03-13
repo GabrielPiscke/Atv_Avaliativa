a388940b20f706bc8a9145718cd1df41637f0f51
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-13 13:56:28 -0300

    upd1

---
+++ avaliacao_pbe/src/main/java/com/example/avaliacao_pbe/AvaliacaoPbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.avaliacao_pbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoPbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoPbeApplication.class, args);
+	}
+
+}
